a1894e156565481d7db772d1ea14bbafeac985cc
youpeng huang <CZ1CG1\youpeng.huang@a-1pxqvrynloxai.ad.openearth.community>
2020-05-06 18:30:50 -0500

    add sanantone weather endpoint

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.listen(port, () => console.log(`Weather app is listening on port ${port}!`))
 const youversion = require('./src/bible/bibleApi.js')
 app.get('/BibleToday', (req, res) => {
 	youversion.todayverse((error, result) => {
+		console.log('Called BibleToday2');
 		console.log(result)
 		res.send(JSON.parse(result).verse.text);
 	})
@@ -37,13 +38,30 @@ app.get('/BibleToday', (req, res) => {
 
 app.get('/BibleToday2', (req, res) => {
 	youversion.todayverse((error, result) => {
-		console.log('Called BibleToday2 succeed, sending index.html');
+		console.log('Called BibleToday2');
 		res.sendFile(path.join(__dirname, 'src/bible/', 'index.html'));
 	})
 })
 
 app.get('/weather2', (req, res) => {
-	console.log('Called weather2 succeed, sending index.html');
+	console.log('Called weather2');
 	res.sendFile(path.join(__dirname, 'src/weather/', 'index.html'));
 })
 
+app.get('/Sanantone', (req, res) => {
+	got(`https://api.darksky.net/forecast/${key}/29.548407,-98.563371`)
+	.then(response => {
+		console.log('Called darksky api succeed.')
+		const msg = JSON.parse(response.body)
+		res.send(msg.hourly.summary)
+	})
+	.catch(error => {
+		console.log(error)
+		res.send(`Call failed with error: ${error}`)
+	})
+})
+
+app.get('/Sanantone2', (req, res) => {
+	console.log('Called San Antone');
+	res.sendFile(path.join(__dirname, 'src/sanantone/', 'index.html'));
+})

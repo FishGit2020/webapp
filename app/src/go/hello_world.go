514bf22bc110089e458b276d015cc5a654a87cc4
youpeng huang-halliburton.com <youpeng.huang@CSW200617192001.ad.openearth.community>
2020-06-29 19:12:17 +0000

    update port and javaserver

--- app/src/go/hello_world.go
+++ app/src/go/hello_world.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        log.Print("Request received!")
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Request received!")
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
 
-    log.Print("Server running on port 8081!")
-    http.ListenAndServe(":8081", nil)
+	log.Print("Server running on port 8080!")
+	http.ListenAndServe(":8080", nil)
 }
